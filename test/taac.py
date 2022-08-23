dd53c83f671b35d8340a6c6f74980e6824a2d777
sangin1 <tkwk9gh@naver.com>
2022-08-23 17:45:01 +0900

    로그인 키패드 추가

--- test/taac.py
+++ test/taac.py
@@ -12,6 +12,7 @@ from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
 import ast
+from hangul_utils import join_jamos
 
 def resource_path(relative_path):
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
@@ -25,6 +26,11 @@ form_word1window = uic.loadUiType(form_word1)[0]
 
 form_word2 = resource_path('word2.ui')
 form_word2window = uic.loadUiType(form_word2)[0]
+
+login_id = resource_path('loginid.ui')
+loin_id_window = uic.loadUiType(login_id)[0]
+
+idnum=0
 
 #mainpage
 class WindowClass(QMainWindow,QWidget, form_class):
@@ -64,7 +70,138 @@ class WindowClass(QMainWindow,QWidget, form_class):
                 self.second = word1window()    
                 self.second.show()
                 self.close()
+            elif self.cus == 3:
+                self.login = loginidwindow()   
+                self.login.show()
+                self.close()
 
+#로그인(아이디)
+class loginidwindow(QDialog,QWidget,loin_id_window):
+    word_table = [['ㄱ','ㄴ','ㄷ','ㄹ','ㅁ','ㅂ','ㅅ'],
+                  ['ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'],
+                  ['ㄲ','ㄸ','ㅃ','ㅆ','ㅉ'],
+                  ['ㅏ','ㅓ','ㅗ','ㅜ','ㅡ','ㅣ','ㅒ'],
+                  ['ㅑ','ㅕ','ㅛ','ㅠ','ㅐ','ㅔ','ㅖ']]
+    cons_all= {'r':'ㄱ', 'R':'ㄲ', 's':'ㄴ', 'e':'ㄷ', 'E':'ㄸ', 'f':'ㄹ', 'a':'ㅁ', 'q':'ㅂ', 'Q':'ㅃ', 't':'ㅅ', 'T':'ㅆ',
+           'd':'ㅇ', 'w':'ㅈ', 'W':'ㅉ', 'c':'ㅊ', 'z':'ㅋ', 'x':'ㅌ', 'v':'ㅍ', 'g':'ㅎ','k':'ㅏ', 'o':'ㅐ', 'i':'ㅑ', 'O':'ㅒ', 'j':'ㅓ', 'p':'ㅔ', 'u':'ㅕ', 'P':'ㅖ', 'h':'ㅗ', 'hk':'ㅘ', 'ho':'ㅙ', 'hl':'ㅚ',
+           'y':'ㅛ', 'n':'ㅜ', 'b':'ㅠ',  'm':'ㅡ', 'l':'ㅣ'}
+               
+    x=1
+    y=0
+    text=''
+    label_text=''
+    border_s = 'font: 20pt "AcadEref"; border-color: rgb(0,21,209); \nbackground-color: rgb(170, 242, 248);\nborder-style: solid;\nborder-width: 2px;\nborder-radius: 10px;'
+    border_b = 'font: 20pt "AcadEref"; border-color: rgb(170,242,248); \nbackground-color: rgb(170, 242, 248);\nborder-style: solid;\nborder-width: 2px;\nborder-radius: 10px;'
+    border_g = 'background-color: rgb(0, 255, 0);\nborder-radius: 30px;'
+    border_gs = 'background-color: rgb(0, 255, 0);\nborder-radius: 30px;border-color: rgb(0,21,209);border-style: solid;\nborder-width: 2px'
+    border_rs = 'background-color: rgb(255, 69, 69);\nborder-radius: 30px;border-color: rgb(0,21,209);border-style: solid;\nborder-width: 2px'
+    border_r = 'background-color: rgb(255, 69, 69);\nborder-radius: 30px;'
+    border_blues = 'background-color: rgb(85, 170, 255);\nborder-radius: 30px;border-color: rgb(0,21,209);border-style: solid;\nborder-width: 2px'
+    border_blue = 'background-color: rgb(85, 170, 255);\nborder-radius: 30px;'
+    def __init__(self):
+        super(loginidwindow,self).__init__()
+        self.initUi()
+        self.label_list[1][0].setStyleSheet(self.border_s)
+        self.show()
+
+    def initUi(self):
+        self.setupUi(self)
+        self.label_list=[]
+        row_list_init = [self.label_2,self.label_3,self.label_4]
+        self.label_list.append(row_list_init)
+        formlayout = QGridLayout()
+        for i in range(5):
+            row_list=[]
+            for j in range(len(self.word_table[i])):
+                label=QLabel(self.word_table[i][j])
+                label.setStyleSheet(self.border_b)
+                formlayout.addWidget(label,i,j)
+                
+                row_list.append(label)
+            self.label_list.append(row_list)
+        self.widget.setLayout(formlayout) 
+
+    def keyPressEvent(self,e):
+        if e.key() == Qt.Key_G:
+            self.second = WindowClass()    
+            self.second.show()
+            self.close()
+        elif e.key() == Qt.Key_S:
+            if self.x < len(self.label_list)-1 and self.y < len(self.label_list[self.x+1]):
+                if self.x==0 and self.y==0:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_g)
+                elif self.x==0 and self.y==1:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_r)
+                elif self.x==0 and self.y==2:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_blue)
+                else:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_b)
+                self.x+=1
+                self.label_list[self.x][self.y].setStyleSheet(self.border_s)
+        elif e.key() == Qt.Key_W:
+            if self.x > 0 and self.y < len(self.label_list[self.x-1]):
+                self.label_list[self.x][self.y].setStyleSheet(self.border_b)
+                self.x-=1
+                if self.x==0 and self.y==0:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_gs)
+                elif self.x==0 and self.y==1:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_rs)
+                elif self.x==0 and self.y==2:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_blues)
+                else:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_s)
+        elif e.key() == Qt.Key_D:
+            if self.y == len(self.label_list[self.x])-1:
+                self.label_list[self.x][self.y].setStyleSheet(self.border_b)
+                self.x=0
+                self.y=0
+                self.label_list[self.x][self.y].setStyleSheet(self.border_gs)
+            if self.y < len(self.label_list[self.x])-1:
+                if self.x==0:
+                    if self.y==0:
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_g)
+                        self.y+=1
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_rs)
+                    elif self.y==1:
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_r)
+                        self.y+=1
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_blues)
+                else:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_b)
+                    self.y+=1
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_s)
+        elif e.key() == Qt.Key_A:
+            if self.y==0:
+                self.label_list[self.x][self.y].setStyleSheet(self.border_b)
+                self.x=0
+                self.y=0
+                self.label_list[self.x][self.y].setStyleSheet(self.border_gs)
+            if self.y > 0:
+                if self.x==0:
+                    if self.y==1:
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_r)
+                        self.y-=1
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_gs)
+                    elif self.y==2:
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_blue)
+                        self.y-=1
+                        self.label_list[self.x][self.y].setStyleSheet(self.border_rs)
+                else:
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_b)
+                    self.y-=1
+                    self.label_list[self.x][self.y].setStyleSheet(self.border_s)
+        elif e.key() == Qt.Key_F:
+            if self.x >0:
+                self.text = self.word_table[self.x-1][self.y]
+                for key, value in self.cons_all.items():
+                    if value == self.text:
+                        self.label_text += key
+                self.label.setText(jamo3(self.label_text))
+                
+            elif self.x == 0 and self.y == 1:
+                self.text = ''
+                self.label_text = ''
+                self.label.setText(self.text)
 
 #분류선택
 class word1window(QDialog,QWidget,form_word1window):
@@ -73,7 +210,6 @@ class word1window(QDialog,QWidget,form_word1window):
     image_name=[]
     x=0
     y=0
-    pos=0 #스크롤 위치
     border_s = 'font: 20pt "AcadEref"; border-color: rgb(0,21,209); \nbackground-color: rgb(170, 242, 248);\nborder-style: solid;\nborder-width: 2px;\nborder-radius: 10px;'
     border_b = 'font: 20pt "AcadEref"; border-color: rgb(170,242,248); \nbackground-color: rgb(170, 242, 248);\nborder-style: solid;\nborder-width: 2px;\nborder-radius: 10px;'
 
@@ -89,16 +225,21 @@ class word1window(QDialog,QWidget,form_word1window):
         self.setupUi(self)
         self.label_list=[]
         formlayout = QGridLayout()
-        #for index in range(100):
-            #formlayout.addRow("button_{}".format(index), QPushButton(str(index)))
         for i in range(len(self.fract_name)):
             row_list=[]
-            for j in range(len(self.fract_name[i])):
+            for j in range(len(self.fract_name[i])):        
                 label=QLabel(self.fract_name[i][j])
+                label.setAlignment(Qt.AlignCenter)
                 label.setStyleSheet(self.border_b)
                 formlayout.addWidget(label,i,j)
                 row_list.append(label)
+                
             self.label_list.append(row_list)
+            
+        if 2 < len(self.label_list):
+                for i in range(3,len(self.label_list)):
+                    for j in range(len(self.label_list[i])):
+                        self.label_list[i][j].hide()
                 
         widget = QWidget()
         widget.setLayout(formlayout) 
@@ -109,7 +250,7 @@ class word1window(QDialog,QWidget,form_word1window):
         for i in range(3):
             if i == 0:
                 check=0
-                sock.sendall(bytes(str(i+1)+"--aa\n",'UTF-8'))
+                sock.sendall(bytes(str(i+1)+"--"+str(idnum)+"\n",'UTF-8'))
                 data = sock.recv(1000000)
                 list2 = []
                 a = str(data.decode())
@@ -128,7 +269,7 @@ class word1window(QDialog,QWidget,form_word1window):
             elif i == 1:
                 check=0
                 check_out = 0
-                sock.sendall(bytes(str(i+1)+"--aa\n",'UTF-8'))
+                sock.sendall(bytes(str(i+1)+"--"+str(idnum)+"\n",'UTF-8'))
                 data = sock.recv(1000000)
                 list = []
                 list_add = []
@@ -167,7 +308,7 @@ class word1window(QDialog,QWidget,form_word1window):
                 check3=0
                 check_out3 = 0
                 a=''
-                sock.sendall(bytes(str(i+1)+"--aa\n",'UTF-8'))
+                sock.sendall(bytes(str(i+1)+"--"+str(idnum)+"\n",'UTF-8'))
                 data2 = sock.recv(100000)
                 while True:
                     data = sock.recv(100000)
@@ -219,35 +360,51 @@ class word1window(QDialog,QWidget,form_word1window):
             if self.x < len(self.label_list)-1 and self.y < len(self.label_list[self.x+1]):
                 self.label_list[self.x][self.y].setStyleSheet(self.border_b)
                 self.x+=1
+
+                if len(self.label_list) > 2:
+                    for i in range(0,len(self.label_list)):
+                        for j in range(len(self.label_list[i])):
+                            self.label_list[i][j].hide()
+
+                    for i in range(self.x,self.x+2):
+                        if i < len(self.label_list):
+                            for j in range(len(self.label_list[i])):
+                                self.label_list[i][j].show()
+                else:
+                    for i in range(2):
+                        for j in range(len(self.label_list[i])):
+                            self.label_list[i][j].show()
+                            
                 self.label_list[self.x][self.y].setStyleSheet(self.border_s)
-                if self.pos < 250:
-                    self.pos+=50
-                    self.scrollArea.verticalScrollBar().setSliderPosition(self.pos);
         elif e.key() == Qt.Key_W:
             if self.x > 0:
                 self.label_list[self.x][self.y].setStyleSheet(self.border_b)
                 self.x-=1
+                if self.y >= len(self.label_list[self.x]):
+                    self.y = len(self.label_list[self.x])-1                    
+               
                 self.label_list[self.x][self.y].setStyleSheet(self.border_s)
-                if self.pos > 0:
-                    self.pos-=20
-                    self.scrollArea.verticalScrollBar().setSliderPosition(self.pos);
+                for i in range(0,len(self.label_list)):
+                    for j in range(len(self.label_list[i])):
+                        self.label_list[i][j].hide()
+                for i in range(self.x,self.x+2):
+                    if i < len(self.label_list):
+                        for j in range(len(self.label_list[i])):
+                            self.label_list[i][j].show()
+                    
         elif e.key() == Qt.Key_D:
             if self.y < len(self.label_list[self.x])-1:
                 self.label_list[self.x][self.y].setStyleSheet(self.border_b)
                 self.y+=1
                 self.label_list[self.x][self.y].setStyleSheet(self.border_s)
         elif e.key() == Qt.Key_A:
-            if self.y > 0:
                 self.label_list[self.x][self.y].setStyleSheet(self.border_b)
                 self.y-=1
                 self.label_list[self.x][self.y].setStyleSheet(self.border_s)
         elif e.key() == Qt.Key_F:
-            #self.hide()
             self.word2 = word2window(self.word_name,self.image_name,self.x,self.y)
             self.word2.show()
             self.close()
-            #self.word2.exec()
-            #self.show()
 
 #단어선택 및 출력
 class word2window(QDialog,QWidget,form_word2window):    
@@ -350,15 +507,16 @@ class word2window(QDialog,QWidget,form_word2window):
                 for i in range(1,len(self.label_list)):
                     for j in range(len(self.label_list[i])):
                         self.label_list[i][j].hide()
-
+                
                 for i in range(self.x,self.x+2):
                     if i < len(self.label_list):
                         for j in range(len(self.label_list[i])):
                             self.label_list[i][j].show()
-                print(1)
-                if self.x == len(self.label_list)-1 and self.x > 1:
-                    for j in range(len(self.label_list[self.x-1])):
-                            self.label_list[self.x-1][j].show()
+
+                if len(self.label_list) > 2:
+                    if self.x == len(self.label_list)-1 and self.x > 1:
+                        for j in range(len(self.label_list[self.x-1])):
+                                self.label_list[self.x-1][j].show()
                             
                 self.label_list[self.x][self.y].setStyleSheet(self.border_s)
         elif e.key() == Qt.Key_W:
@@ -438,7 +596,52 @@ class word2window(QDialog,QWidget,form_word2window):
                 self.text = ''
                 self.text_label.setText(self.text)
         
-
+def jamo3(text2):
+    cons = {'r':'ㄱ', 'R':'ㄲ', 's':'ㄴ', 'e':'ㄷ', 'E':'ㄸ', 'f':'ㄹ', 'a':'ㅁ', 'q':'ㅂ', 'Q':'ㅃ', 't':'ㅅ', 'T':'ㅆ',
+           'd':'ㅇ', 'w':'ㅈ', 'W':'ㅉ', 'c':'ㅊ', 'z':'ㅋ', 'x':'ㅌ', 'v':'ㅍ', 'g':'ㅎ'}
+    vowels = {'k':'ㅏ', 'o':'ㅐ', 'i':'ㅑ', 'O':'ㅒ', 'j':'ㅓ', 'p':'ㅔ', 'u':'ㅕ', 'P':'ㅖ', 'h':'ㅗ', 'hk':'ㅘ', 'ho':'ㅙ', 'hl':'ㅚ',
+           'y':'ㅛ', 'n':'ㅜ', 'nj':'ㅝ', 'np':'ㅞ', 'nl':'ㅟ', 'b':'ㅠ',  'm':'ㅡ', 'ml':'ㅢ', 'l':'ㅣ'}
+    cons_double = {'rt':'ㄳ', 'sw':'ㄵ', 'sg':'ㄶ', 'fr':'ㄺ', 'fa':'ㄻ', 'fq':'ㄼ', 'ft':'ㄽ', 'fx':'ㄾ', 'fv':'ㄿ', 'fg':'ㅀ', 'qt':'ㅄ'}
+    cons_all= {'r':'ㄱ', 'R':'ㄲ', 's':'ㄴ', 'e':'ㄷ', 'E':'ㄸ', 'f':'ㄹ', 'a':'ㅁ', 'q':'ㅂ', 'Q':'ㅃ', 't':'ㅅ', 'T':'ㅆ',
+           'd':'ㅇ', 'w':'ㅈ', 'W':'ㅉ', 'c':'ㅊ', 'z':'ㅋ', 'x':'ㅌ', 'v':'ㅍ', 'g':'ㅎ','k':'ㅏ', 'o':'ㅐ', 'i':'ㅑ', 'O':'ㅒ', 'j':'ㅓ', 'p':'ㅔ', 'u':'ㅕ', 'P':'ㅖ', 'h':'ㅗ', 'hk':'ㅘ', 'ho':'ㅙ', 'hl':'ㅚ',
+           'y':'ㅛ', 'n':'ㅜ', 'b':'ㅠ',  'm':'ㅡ', 'l':'ㅣ'}
+    result = ''
+    vc = ''
+    for t in text2:
+        if t in cons :
+            vc+='c'
+        elif t in vowels:
+            vc+='v'
+        else:
+            vc+='!'
+    vc = vc.replace('cvv', 'fVV').replace('cv', 'fv').replace('cc', 'dd')
+    i = 0
+    while i < len(text2):
+        v = vc[i]
+        t = text2[i]
+        j = 1
+        try:
+            if v == 'f' or v == 'c':  
+                result+=cons[t]
+            elif v == 'V':
+                result+=vowels[text2[i:i+2]]
+                j+=1
+            elif v == 'v':
+                result+=vowels[t]
+            elif v == 'd':
+                result+=cons_double[text2[i:i+2]]
+                j+=1
+            else:
+                result+=t
+        except:
+            print(t)
+            if t in cons_all:
+                result+=cons_all[t]
+            else:
+                result+=t                   
+        i += j
+    return join_jamos(result)
+    
 if __name__ == '__main__':
     sock = socket(AF_INET,SOCK_STREAM)
     sock.connect(('localhost',6000));
